94563b49bd35cc3642920cfcadda428beb55d033
Ryan Holcombe <rholcombe30@gmail.com>
2021-11-02 13:38:47 -0600

    :shirt: add comment

--- dao/db.go
+++ dao/db.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+// NewPostgresDB opens a new postgres connection
 func NewPostgresDB(dataSourceName string) *sqlx.DB {
 	return sqlx.MustOpen("postgres", dataSourceName)
 }
